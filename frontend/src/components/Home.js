a5322c68d08e3ed2b621ee199005dc959a3f42b0
inessearasilva <inessilvaaaaa0@gmail.com>
2023-06-02 14:28:54 +0100

    table download

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Table } from '@nextui-org/react';
+
 
 function BrandExample() {
   return (
@@ -10,7 +12,6 @@ function BrandExample() {
         <div class="container text">
         <div class="row align-items-start">
           <div class="col-4">
-            One column of 1/3 Hello. I am tetsing these columns in order to see if they 
           </div>
           <div class="col-8">
             One column of 2/3. Despite the size being here. it is fixed
