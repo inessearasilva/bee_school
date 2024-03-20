d548394edcf1a210e5992142c8b7e6bce4e295b4
inessearasilva <inessilvaaaaa0@gmail.com>
2024-03-20 19:41:44 +0000

    u

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://bee-aware-08ab5a1c994e.herokuapp.com/" || "http://localhost:5050/";
+const baseURL = "https://bee-aware-08ab5a1c994e.herokuapp.com/api" || "http://localhost:5050/api";
 
 export default axios.create({
   baseURL: baseURL,
