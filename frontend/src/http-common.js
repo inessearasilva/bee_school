6d56dab56631ffb564ad80e38f2b424a7d583669
inessearasilva <inessilvaaaaa0@gmail.com>
2024-03-20 18:46:37 +0000

    up

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = "https://bee-aware-08ab5a1c994e.herokuapp.com/api" || "http://localhost:5050/api";
+
 export default axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: baseURL,
   headers: {
     "Content-type": "application/json"
   }
--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Router = require("./app/routes/tweets")
 const cors = require("cors");
-const path = require('path');
 
 const app = express();
 
-var corsOptions = {
-  origin: "https://bee-aware-08ab5a1c994e.herokuapp.com/"
-};
-
-app.use(cors(corsOptions));
+// Set CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace * with your allowed origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(cors()); // Add this line to enable CORS
 
 require("dotenv").config();
 
@@ -29,15 +32,6 @@ db.once("open", function () {
 
 app.use(Router);
 
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
-// Serve static files from the 'frontend/build' directory
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-// This route should serve 'index.html' for all routes in the frontend
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+app.listen(5050, () => {
+  console.log("Server is running at port 5050");
 });
